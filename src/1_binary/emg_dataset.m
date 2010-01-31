739ba0b3e6e42636c227656d42b032f1dff2c872
Nurettin Senyer <nurettin.senyer@bil.omu.edu.tr>
2010-01-31 03:37:58 +0200

    dos2lin, magic sayilardan kurtulduk, `dir` islevinin paterni guncellendi
    1. dos2lin: db isimleri genelde dos formatinda gel
       mekteydi, linux altinda calisirken bunu aktive etmek gerekiyor
    2. magic sayilar: orn. "kapaliya ait hareket 75000-10000 arasinda"
    3. `dir` islevi: normalde dir(dnm) iken dir(strcat(dnm, 'm*.mat'))

--- src/1_binary/emg_dataset.m
+++ src/1_binary/emg_dataset.m
@@ -1,49 +1,59 @@
 function veri = emg_dataset();
 % EMG veri setini uret
+% 
+% Aciklama:
+% 
+% 1. binary siniflandirma 
+% 2. veri setinde dort (4) hareket var (HAREKET_SAYISI = 4)
+% 3. veri setinden sadece "kapali (2)" ve "serbest (3)" durumlar alinacak
+%    `DB_ROOT/readme` dosyasina bakin
+HAREKET_SAYISI = 4;
+
 DB_ROOT = '/home/nurettins/proje/db/emg/4hareket/islenmis/';
-dbtrnm  = strcat(DB_ROOT, dos2lin('data\train\'));
-dbtstnm = strcat(DB_ROOT, dos2lin('data\test\'));
+dbtrnm  = strcat(DB_ROOT, dos2lin('train\'));
+dbtstnm = strcat(DB_ROOT, dos2lin('test\'));
 
-emg_dataset2(dbtrnm);
-emg_dataset2(dbtstnm);
-
-function emg_dataset2(dbnm) 
-T_KAPALI = 50000;
-T_ACIK   = 75000;
 dbg = false;
+emg_dataset2(dbtrnm,  HAREKET_SAYISI, dbg);
+emg_dataset2(dbtstnm, HAREKET_SAYISI, dbg);
+
+function emg_dataset2(dbnm, HAREKET_SAYISI, dbg)
 
 if dbg, clc;    close all; end
 
-% El kapali bilgisi: 5000-7500  ornek arasinda 
-% El acik bilgisi:   7500-10000 ornek arasinda 
+data.Etiket = {'KAPALI', 'SERBEST'};
+KAPALI  = 1;
+SERBEST = 2;
 
-DIR = dir(dbnm);
-sz = length(DIR);
+% dosya paterni ('m*.mat') icin `DB_ROOT/readme` bakin
+DIR = dir(strcat(dbnm, 'm*.mat'));
+fsz = length(DIR);
 j = 0;
 
-for i=1:sz
-    if strcmp(DIR(i).name, '.') | strcmp(DIR(i).name, '..') | ...
-       strcmp(DIR(i).name, 'veriler.txt') | ...
-       strcmp(DIR(i).name, 'EmgTstData.mat') | ...
-       strcmp(DIR(i).name, 'EmgTrnData.mat'), 
-       continue;  end        
-    
+for i=1:fsz
     fnm = strcat(dbnm, DIR(i).name);
     tmp = load(fnm);
     
-    data_kapali = tmp.data(T_KAPALI:T_ACIK, 2);
-    data_acik = tmp.data((T_ACIK) :(T_ACIK+length(data_kapali)-1), 2);
+    % kapali ve serbest durumlarin koordinatlarini al
+    % dosyanin girisindeki aciklamayi oku
+    dsz = size(tmp.data, 1);
+    W = floor(dsz/HAREKET_SAYISI);
+    T_KAPALI  = 2 * W + 1;
+    T_SERBEST = 3 * W + 1;
+
     
-    j = j + 1;  data.X(:, j) = data_acik';       data.y(j) = 1;
-    j = j + 1;  data.X(:, j) = data_kapali';     data.y(j) = 2;  
+    data_kapali  = tmp.data(T_KAPALI:(T_SERBEST-1), 2);
+    data_serbest = tmp.data((T_SERBEST) :(T_SERBEST+length(data_kapali) - 1), 2);
+    
+    data.X(:, i) = data_kapali';    data.y(i) = KAPALI;  
+    data.X(:, i) = data_serbest';   data.y(i) = SERBEST;
  
     if dbg
-        subplot(211),   plot(tmp.data(:,1));    axis([0 100000 -2 2]);  grid on;
-        subplot(212),   plot(tmp.data(:,2));    axis([0 100000 -2 2]);  grid on;
-        %subplot(211),   plot(data_acik);    title('ACIK');      axis([0 2.5e4 -2 2]);
-        %subplot(212),   plot(data_kapali);  title('KAPALI');    axis([0 2.5e4 -2 2]);
+        subplot(211),   plot(tmp.data(:,1));    axis([0 dsz -2 2]);  grid on;
+        subplot(212),   plot(tmp.data(:,2));    axis([0 dsz -2 2]);  grid on;
+        %subplot(211),   plot(data_kapali);  title('KAPALI');    axis([0 2.5e4 -2 2]);
+        %subplot(212),   plot(data_serbest); title('SERBEST');   axis([0 2.5e4 -2 2]);
         pause(.3)
     end
 end
-data.Etiket = {'ACIK', 'KAPALI'};
 savedb(dbnm, data);
